b65361d6a44ea5ad2dd3c1ea2f9a12c0822be119
Mamoru Honda <honda@3-ize.jp>
2014-04-19 14:52:56 +0900

    始めのコミット。GemとRubyとRailsと。

---
+++ lpis/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lpis::Application.config.secret_key_base = 'e1e910f6aa3e8dc666aba1784379606f49ab3e59a6f63d62e7fe3eb4b2bab278c583fe532a91effe9320c1a4e7854847bfe0a072ccfe9b7e89f9e6de0ffb1de2'
